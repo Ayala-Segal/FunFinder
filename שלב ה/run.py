1ad13c765e488aeaaf10ad839eeaacfeea05a193
AyeletShira <ayeletss04@gmail.com>
2026-06-30 20:26:34 +0300

    Show clickable localhost link in terminal on startup

--- שלב ה/run.py
+++ שלב ה/run.py
@@ -3,4 +3,8 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    print("\n" + "="*50)
+    print("  FunFinder is running!")
+    print("  Open in browser: http://localhost:5000")
+    print("="*50 + "\n")
+    app.run(debug=False, host='0.0.0.0', port=5000)
